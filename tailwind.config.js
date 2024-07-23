a1598f1e796bb5a8b125b6ee25c4858cfe377b20
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-07-23 15:33:05 +0200

    adding colors + config files and src files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tiaMaria: "#C73A0F",
+        mountainMeadow: "#1EA475",
+        vistaWhite: "#FCF9F7",
+        softPeach: "#F4EDEB",
+        clamShell: "#C9AEA6",
+        pharlap: "#AD8985",
+        beaver: "#87635A",
+        graphite: "#260F08",
+      },
+    },
   },
   plugins: [],
-}
-
+};
