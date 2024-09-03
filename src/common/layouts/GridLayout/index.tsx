782077539f4545952e95a7a36186f4666d5366a9
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-09-03 11:20:40 +0200

    try to fix card responsive UI

--- src/common/layouts/GridLayout/index.tsx
+++ src/common/layouts/GridLayout/index.tsx
@@ -1,7 +1,7 @@
 import { LayoutProps } from "../../definition";
 
 const Layout = ({ children }: LayoutProps) => (
-  <div className="mt-12 grid grid-cols-1 gap-10 md:grid-cols-2 lg:mt-16 lg:grid-cols-3">
+  <div className="mt-12 grid grid-cols-1 gap-10 md:grid-cols-2 lg:mt-16 2xl:grid-cols-3">
     {children}
   </div>
 );
